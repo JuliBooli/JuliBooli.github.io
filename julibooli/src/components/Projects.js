c216194c41b84644116db530713fe282401d36fc
Julian <julian.aschwanden@lernende.bbw.ch>
2025-01-24 18:09:02 +0100

    Update Projects and their style

--- julibooli/src/components/Projects.js
+++ julibooli/src/components/Projects.js
@@ -1,13 +1,36 @@
-function Projects() {
+import './css/projects.css';
 
+function Projects() {
     return (
         <>
             <h1>Projects</h1>
-            <a href="https://github.com/JuliBooli/box-clicker" onClick="openInNewTab">
-                box-clicker
-            </a>
+            <div className="projectLink">
+                <a
+                    className="link"
+                    href="https://github.com/JuliBooli/box-clicker"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    box-clicker
+                </a>
+            </div>
+
+            <div className="projectLink">
+                <a
+                    className="link"
+                    href="https://github.com/JuliBooli/three-js-fun"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    three js
+                </a>
+                <div>
+                    <h3>Game of Life</h3>
+                    <img src="/assets/img.png" alt="Game of Life preview"/>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
 export default Projects;
